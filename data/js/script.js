c610b7613a7d8027da0e8a1003a15548f7327c55
µKöff <muekoeff@muekev.de>
2019-05-29 23:44:21 +0200

    Better support for ^-condition

--- data/js/script.js
+++ data/js/script.js
@@ -143,25 +143,55 @@ class RequestQueue {
 		tuple.ui.update();
 
 		if(tuple.conditions.length == 1 && tuple.conditions[0].mode == "^") {
-			// @TODO
-			$.ajax(`https://de.wikipedia.org/w/api.php?action=query&format=json&prop=pageprops&ppprop=wikibase_item&redirects=1&titles=${encodeURIComponent(tuple.searchquery)}&origin=*`
-			).always(function(e) {
-				this_rq._finishRequest();
-			}).done(function(e) {
-				console.debug(e);
-				if(Object.keys(e.query.pages)[0] == "-1") {
-					tuple.ui.status = "noresults";
+			var host = getHost(tuple.conditions[0].value);
+			if(host != null) {
+				$.ajax({
+					"data": {
+						"action": "query",
+						"format": "json",
+						"prop": "pageprops",
+						"ppprop": "wikibase_item",
+						"redirects": "1",
+						"titles": tuple.searchquery,
+						"origin": "*"
+					},
+					"url": `https://${host}/w/api.php`
+				}).always(function(e) {
+					this_rq._finishRequest();
+				}).done(function(e) {
+					console.debug(e);
+					if(Object.keys(e.query.pages)[0] == "-1") {
+						tuple.ui.status = "noresults";
+						tuple.ui.update();
+					} else {
+						tuple.ui.status = "success";
+						tuple.result = e.query.pages[Object.keys(e.query.pages)[0]].pageprops.wikibase_item;
+						tuple.ui.update();
+					}
+				}).fail(function(e) {
+					tuple.ui.status = "failed";
 					tuple.ui.update();
-				} else {
-					tuple.ui.status = "success";
-					tuple.result = e.query.pages[Object.keys(e.query.pages)[0]].pageprops.wikibase_item;
-					tuple.ui.update();
-				}
-			}).fail(function(e) {
-				tuple.ui.status = "failed";
-				tuple.ui.update();
-				console.error(e);
-			});
+					console.error(e);
+				});
+			}
+
+			function getHost(name) {
+				if(name == "commonswiki") return "commons.wikimedia.org";
+				if(name == "mediawikiwiki") return "www.mediawiki.org";
+				if(name == "metawiki") return "meta.wikimedia.org";
+				if(name == "specieswiki") return "species.wikimedia.org";
+				if(name == "wikidatawiki") return "wikidata.org";
+				if(name == "wikimaniawiki") return "wikimania.wikimedia.org";
+
+				var match = name.match(/^([a-zA-Z_]+)(wikibooks|wikinews|wikiquote|wikisource|wikiversity|wikivoyage|wiktionary)$/);
+				if(match != null) return `${match[1]}.${match[2]}.org`;
+
+				var match = name.match(/^([a-zA-Z_]+)wiki$/);
+				if(match != null) return `${match[1]}.wikipedia.org`;
+
+				console.warn("Unknown wiki: ", name);
+				return null;
+			}
 		} else {
 			$.ajax(`https://www.wikidata.org/w/api.php?action=wbsearchentities&format=json&language=${$("#setting-language-search").val()}&limit=${Utils.validateNumber($("#setting-disambiguation-candidatesnumber").val(), 7)}&origin=*&type=${tuple.type.toLowerCase() == "q" ? "item" : "property"}&search=${tuple.searchquery}`
 			).always(function(e) {
